b0ae1418ad01172013426566e3d501f353d4d1fc
Seanmclem <seanmclements@gmail.com>
2021-12-31 23:09:51 -0500

    some comments idk

--- src/services/PTYService.ts
+++ src/services/PTYService.ts
@@ -21,7 +21,7 @@ export class PTYService {
    * Spawn an instance of pty with a selected shell.
    */
   startPtyProcess() {
-    console.log({ pty });
+    // console.log({ pty });
     this.ptyProcess = pty.spawn(this.shell, [], {
       name: "xterm-color",
       cwd: process.env.HOME, // Which path should terminal start
@@ -32,6 +32,9 @@ export class PTYService {
     this.ptyProcess.on("data", (data: any) => {
       // deprecated, use onData
       // Whenever terminal generates any data, send that output to socket.io client
+      // *this is like, the result/response sent to the terminal as output
+      //   console.log("onData", data);
+
       this.sendToClient(data);
     });
   }
@@ -40,15 +43,16 @@ export class PTYService {
    * Use this function to send in the input to Pseudo Terminal process.
    * @param {*} data Input from user like a command sent from terminal UI
    */
-
   write(data: string) {
     if (this.ptyProcess) {
-      this.ptyProcess.write(data);
+      this.ptyProcess.write(data); // "ls\r"
     }
   }
 
   sendToClient(data: string) {
     // Emit data to socket.io client in an event "output"
+    // *this is like the output of what you type, not the result of sending it
+    // console.log("output", data);
     this.socket.emit("output", data);
   }
 }
